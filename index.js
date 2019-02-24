7cd4e4a66e0faa1bee5b2a994d107c5a4e29a053
Veronica Miller <vmcmiller@gmail.com>
2019-02-24 20:21:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  };
+
+  veto = function() {
+    return "No, I must agree";
+  };
+
+  approve = function() {
+    return "You can do that!";
+  };
+
+  doCharity = function() {
+    return "I like to help people.";
+  };
+
+  releasePressStatement = function() {
+    return "You will see great things from Scuber.";
+  };
+
+  sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  };
+
+};
+
+
+
+
+
+  
+}
